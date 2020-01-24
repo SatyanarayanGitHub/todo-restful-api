795a7a65f836978c54e4e16b2c8088675aaade3c
Satyanarayan Yadav <satyanarayan.y@gmail.com>
2020-01-24 11:19:18 +0530

    Initial Commit

---
+++ src/test/java/org/srysoft/todo/TodoRestfulApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.srysoft.todo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoRestfulApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
